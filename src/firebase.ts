e45e54742e92effd5a26d18d418c359b2e2b1610
MINT <pb1123love@gmail.com>
2023-10-18 20:37:22 +0900

    ✨ Feat(Firebase Setup): install package, initializing firebase
    #

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcsWyFojx8rVr1nNfecl6FRBpTOJaKJBY",
+  authDomain: "react-x-f4ae1.firebaseapp.com",
+  projectId: "react-x-f4ae1",
+  storageBucket: "react-x-f4ae1.appspot.com",
+  messagingSenderId: "737569960590",
+  appId: "1:737569960590:web:1c87c796b78d4504fa0fdf",
+};
+
+const app = initializeApp(firebaseConfig);
