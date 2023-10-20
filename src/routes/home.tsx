0997e10cf7f05683484956beb099eba11bf7357d
김민규 <minkyu.kim@birdview.kr>
2023-10-20 13:10:42 +0900

    ✨ Feat(Authentication): develop ProtectedComponent
    #

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,9 @@
+import { auth } from "../firebase";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const LogOut = () => {
+    auth.signOut();
+  }
+
+  return <h1><button onClick={LogOut}>Log Out</button></h1>;
 }
