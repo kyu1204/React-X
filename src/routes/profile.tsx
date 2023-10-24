b5d5f736f2bcaf5501f523462770f463127e1860
김민규 <minkyu.kim@birdview.kr>
2023-10-24 15:24:08 +0900

    ✨ Feat: add my tweets timeline to profile
    #

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
-import { auth, storage } from "../firebase";
-import { useState } from "react";
+import { auth, db, storage } from "../firebase";
+import { useEffect, useState } from "react";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { ITweet } from "../components/timeline";
+import Tweet from "../components/tweet";
 
 const Wrapper = styled.div`
   display: flex;
@@ -39,11 +42,19 @@ const Name = styled.span`
   font-size: 22px;
 `;
 
+const Tweets = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
 const MB = 1 * 1024 * 1024;
 
 export default function Profile() {
   const user = auth.currentUser;
   const [avatar, setAvatar] = useState(user?.photoURL);
+  const [tweets, setTweets] = useState<ITweet[]>([]);
 
   const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
@@ -60,6 +71,27 @@ export default function Profile() {
     }
   };
 
+  const fetchTweets = async () => {
+    const tweetQuery = query(
+      collection(db, "tweets"),
+      where("userId", "==", user?.uid),
+      orderBy("createdAt", "desc"),
+    )
+    const snapshot = await getDocs(tweetQuery);
+    const tweets = snapshot.docs.map(doc => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return {
+        tweet, createdAt, userId, username, photo, id: doc.id,
+      }
+    });
+    setTweets(tweets);
+  }
+
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+
+
   return (
     <Wrapper>
       <AvataUpload htmlFor="avatar">
@@ -70,6 +102,9 @@ export default function Profile() {
       </AvataUpload>
       <AvatarInput onChange={onAvatarChange} id={"avatar"} type="file" accept="image/*" />
       <Name>{user?.displayName ?? "Anonymous"}</Name>
+      <Tweets>
+        {tweets.map(tweet => <Tweet key={tweet.id} {...tweet} />)}
+      </Tweets>
     </Wrapper>
   );
 }
